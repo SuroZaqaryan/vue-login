ece5d1caaa077036cb5387c88ca09ffb43950d44
Synchro <47273452+SuroZaqaryan@users.noreply.github.com>
2022-10-25 02:44:18 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from "./components/Login.vue";
+
+export default {
+  name: "app",
+  components: {
+    Login
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #222;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
